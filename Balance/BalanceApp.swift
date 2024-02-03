3ce2806c7dc7fe5ad68acb9b30d9582d67a5e8b3
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-03 11:44:31 -0600

    Initial Commit

---
+++ Balance/BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceApp.swift
+//  Balance
+//
+//  Created by J. DeWeese on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
